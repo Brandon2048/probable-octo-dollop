07da3e3df2a8dde3e56724cb62ab3ba977070edd
LEGOSROCKDUDE86 <arriagabrandon231@gmail.com>
2019-10-17 09:09:38 -0700

    minimum
    z

--- webapp.py
+++ webapp.py
@@ -1,18 +1,33 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, request
 
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
 @app.route("/")
 def render_main():
-    return render_template('home.html')
+    if('Rupee' in request.args):
+        USDtoWon = float(request.args['Won'])
+        response3 = (USDtoWon) * (1179.46)
+        return render_template('home.html', responseFromServer = response3)
+    else:
+        return render_template('home.html')
 
 @app.route("/p1")
 def render_page1():
-    return render_template('VietnameseDong.html')
+    if('Dong' in request.args):
+        USDtoDong = float(request.args['Dong'])
+        response = (USDtoDong) * (23208)
+        return render_template('VietnameseDong.html', responseFromServer = response)
+    else:
+        return render_template('VietnameseDong.html')
 
 @app.route("/p2")
 def render_page2():
-    return render_template('Rupee.html')
+    if('Rupee' in request.args):
+        USDtoRupee = float(request.args['Rupee'])
+        response1 = (USDtoRupee) * (71.5)
+        return render_template('Rupee.html', responseFromServer = response1)
+    else:
+        return render_template('Rupee.html')
 
 if __name__=="__main__":
     app.run(debug=False)
